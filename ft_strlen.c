477915c005d432cf19349ecbf96223f16aece122
Maxim <maxim.leonet@gmail.com>
2023-03-24 01:31:27 +0100

    Correction norme

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 01:25:19 by mleonet           #+#    #+#             */
+/*   Updated: 2023/03/24 01:28:54 by mleonet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlen(const char *str)
@@ -5,15 +17,9 @@ size_t	ft_strlen(const char *str)
 	size_t	i;
 
 	i = -1;
+	if (str == NULL)
+		return (0);
 	while (str[++i])
 		;
 	return (i);
 }
-
-/*
-#include <stdio.h>
-int	main(int argc, char **argv)
-{
-	if (argc == 2)
-		printf("%zu\n", ft_strlen(&argv[1][0]));
-}*/
