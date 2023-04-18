a6b007374c4846206f0216b1b402c3f127b5cfb9
mleonet <mleonet@student.s19.be>
2023-04-18 16:47:15 +0200

    Fixed ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 13:35:57 by mleonet           #+#    #+#             */
-/*   Updated: 2023/04/13 14:41:22 by mleonet          ###   ########.fr       */
+/*   Updated: 2023/04/18 16:43:33 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = -1;
 	if (!s)
 		return (NULL);
-	if (start > ft_strlen(s))
-	{
+	if (start >= ft_strlen(s))
 		len = 0;
-	}
-	str = malloc(sizeof(char *) * (len + 1));
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	str = malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
 	while (++i < len)
-	{
 		str[i] = s[start + i];
-	}
 	str[i] = '\0';
 	return (str);
 }
