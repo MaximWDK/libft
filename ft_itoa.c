a1c42c2487f074aa37485ba9fdaffbd757ab89c6
mleonet <mleonet@student.s19.be>
2023-04-26 18:14:46 +0200

    Added itoa not finished

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,61 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:55:43 by mleonet           #+#    #+#             */
-/*   Updated: 2023/04/25 13:55:47 by mleonet          ###   ########.fr       */
+/*   Updated: 2023/04/26 15:53:00 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_itoa(int n)
+#include "libft.h"
+
+int	ft_findlen(long int n)
 {
-	
+	int	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		n *= -1;
+		count++;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		count++;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*str;
+	int			save;
+	long int	len;
+
+	save = n;
+	len = ft_findlen(n);
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
+	str[len--] = 0;
+	if (n == 0)
+		str[0] = '0';
+	if (n == -2147483648)
+	{
+		str[0] = '-';
+		str[1] = '2';
+		save = 147483648;
+	}
+	if (save < 0)
+	{
+		str[0] = '-';
+		save = save * -1;
+	}
+	while (save > 0)
+	{
+		str[len--] = save % 10 + '0';
+		save = save / 10;
+	}
+	return (str);
 }
