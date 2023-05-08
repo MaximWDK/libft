1a22f418f59ba223fd5e34e43323ae0cb125385d
Maxim <maxim.leonet@gmail.com>
2023-05-08 16:39:39 +0200

    Atoi opti

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,46 +6,35 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 01:24:36 by mleonet           #+#    #+#             */
-/*   Updated: 2023/05/08 16:06:02 by mleonet          ###   ########.fr       */
+/*   Updated: 2023/05/08 16:38:34 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_sign(const char *str, int *sign)
-{
-	int	i;
-
-	i = 0;
-	*sign = 1;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-')
-	{
-		i++;
-		*sign = -1;
-	}
-	else if (str[i] == '+')
-		i++;
-	return (i);
-}
-
 int	ft_atoi(const char *str)
 {
-	int			i;
 	int			sign;
 	long int	result;
 	long int	temp;
 
-	temp = 0;
 	result = 0;
-	i = ft_sign(str, &sign);
-	while (str[i] >= '0' && str[i] <= '9')
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-' || *str == '+')
 	{
-		result = result * 10 + str[i++] - '0';
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		temp = result;
+		result = result * 10 + *str - '0';
+		str++;
 		if (temp > result && sign > 0)
 			return (-1);
 		else if (temp > result && sign < 0)
 			return (0);
-		temp = result;
 	}
 	return (result * sign);
 }
